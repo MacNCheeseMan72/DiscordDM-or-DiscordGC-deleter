2f134c0d0abddb91b3712aacd911ee707f08816d
JustSkillNoBan <83167227+MacNCheeseMan72@users.noreply.github.com>
2021-09-20 09:07:42 -0400

    just made it better
    added imports, added stopwatch, made it into a function and an input for the token so you dont gotta edit src

--- veryHarm.py
+++ veryHarm.py
@@ -1,15 +1,25 @@
-i = 0
-token = "your token here"
-headers = {'Authorization': token}
-j = requests.get("https://discordapp.com/api/v9/users/@me/channels", headers=headers).json()
-for dm in j:
-   i += 1
-   dm_id = dm["id"]
-   requests.delete(f"https://discordapp.com/api/v9/channels/{dm_id}", headers=headers)
-   print(f"deleted dm / groupchat, id {dm_id}, things deleted : {i}")
+# need to have requests & time pre-installed
+import requests
+import time
 
-start = input("press enter to start :D...")
-deleter()
-close = input(f"closed {i} dm's / gc's.  ty for using me :)   (click enter to close)...")
+def deleter(token):
+    i = 0
+    headers = {'Authorization': token}
+    j = requests.get("https://discordapp.com/api/v9/users/@me/channels", headers=headers).json()
+    for dm in j:
+        i += 1
+        dm_id = dm["id"]
+        requests.delete(f"https://discordapp.com/api/v9/channels/{dm_id}", headers=headers)
+        print(f"deleted dm / groupchat, id {dm_id}, things deleted : {i}")
+
+tok = input("input token : ")
+input("press enter to start :D...")
+start_time = time.time()
+deleter(tok)
+end_time = time.time()
+time_lapsed = end_time - start_time
+input(f"closed {i} dm's / gc's in {time_lapsed}.  ty for using me :)   (click enter to close)...")
 
 # credits to JustSkillNoBan#6646, i coded this lol
+
+# made things better, added a stopwatch (just fixed it p much)
